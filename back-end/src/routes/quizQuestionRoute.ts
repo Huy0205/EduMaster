5fb51d98192e246d45227c68db8a83989e5233f3
LAPTOP-3BEOF0QJ\PC <nguyentanhuy02052000@gmail.com>
2024-12-12 21:52:13 +0700

    Edit authentication in quiz question

--- back-end/src/routes/quizQuestionRoute.ts
+++ back-end/src/routes/quizQuestionRoute.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { QuizQuestionController } from '../app/controllers';
-import { authentication } from '../middlewares';
+// import { authentication } from '../middlewares';
 
 const quizQuestionRouter = Router();
 
 // POST: http://localhost:8080/api/v1/quiz-question/add-multiple
-quizQuestionRouter.post('/add-multiple', authentication, QuizQuestionController.addQuizQuestions);
+// quizQuestionRouter.post('/add-multiple', authentication, QuizQuestionController.addQuizQuestions);
+quizQuestionRouter.post('/add-multiple', QuizQuestionController.addQuizQuestions);
 
 export default quizQuestionRouter;
